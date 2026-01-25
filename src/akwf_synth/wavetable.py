100cfcd276c5051aa5cf2f175903b8964d2cde27
Marco Della Vedova <marcodellav@gmail.com>
2026-01-25 01:27:45 +0000

    add WaveData class

--- src/akwf_synth/wavetable.py
+++ src/akwf_synth/wavetable.py
@@ -1,6 +1,23 @@
+from dataclasses import dataclass
+
 import numpy as np
 
 
-class WaveTable:
-    def __init__(self):
-        pass
+@dataclass(frozen=True)
+class WaveData:
+    samples: np.ndarray
+
+    @property
+    def length(self) -> int:
+        return self.samples.shape[0]
+
+    @property
+    def samples_normalised(self) -> np.ndarray:
+        peak_abs = np.max(np.abs(self.samples))
+
+        if peak_abs == 0 or peak_abs == 1:
+            return self.samples
+        else:
+            coefficient = 1 / peak_abs
+            normalised = self.samples * coefficient
+            return normalised
--- src/loaders/wav_loader.py
+++ src/loaders/wav_loader.py
@@ -1,6 +1,22 @@
+from dataclasses import dataclass
+
 import numpy as np
 import soundfile as sf
 
+from akwf_synth.wavetable import WaveData
 
-def load_data_from_wav_file(path):
-    pass
+
+def load_data_from_wav_file(path: str) -> WaveData:
+    samples, samplerate = sf.read(path, dtype="float32")
+
+    # If the wav file is stereo, take the first channel
+    if samples.ndim > 1:
+        samples = samples[:, 0]
+
+    return WaveData(samples=samples)
+
+
+# wave = load_data_from_wav_file("./data/akwf/AKWF/AKWF_0001/AKWF_0015.wav")
+
+# print(wave.samples)
+# print(wave.samples_normalised)
