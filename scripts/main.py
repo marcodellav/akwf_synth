feb0f31e89b4281820da1bd5e2d387346a60ed1f
Marco Della Vedova <marcodellav@gmail.com>
2026-02-01 19:17:14 +0000

    change the audioengine render form an oscillator to a synth

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,7 @@
 from akwf_synth.audio import AudioEngine
 from akwf_synth.config import AudioConfig
-from akwf_synth.oscillator import WavetableOscillator
-from akwf_synth.synth import MonoSynth
-from loaders.wav_loader import load_data_from_wav_file
 from akwf_synth.input.keyboard import KeyboardController
+from akwf_synth.synth import MonoSynth
 
 """
 main.py
@@ -12,14 +10,9 @@ main.py
 
 
 def main():
-    wave = load_data_from_wav_file("./data/akwf/AKWF/AKWF_0002/AKWF_0130.wav")
     audio_config = AudioConfig()
-    # synth_config = SynthConfig()
     synth = MonoSynth()
-    renderer = WavetableOscillator(
-        waveform=wave, frequency=110, amplitude=1, sampling_rate=44100
-    )
-    engine = AudioEngine(renderer, audio_config)
+    engine = AudioEngine(synth, audio_config)
     kc = KeyboardController(synth.handle_note_event)
     with engine, kc:
         input("Press Enter to quit...\n")
--- src/akwf_synth/synth.py
+++ src/akwf_synth/synth.py
@@ -1,4 +1,6 @@
 from akwf_synth.input.keyboard import NoteOff, NoteOn
+from akwf_synth.oscillator import WavetableOscillator
+from loaders.wav_loader import load_data_from_wav_file
 
 
 def midi_note_to_frequency(midi_note):
@@ -8,21 +10,30 @@ def midi_note_to_frequency(midi_note):
     A4_FREQUENCY = 440.0
     A4_MIDI_NOTE = 69
 
-    return A4_FREQUENCY * 2 ** ((midi_note - A4_MIDI_NOTE) / 12), 2
+    return A4_FREQUENCY * 2 ** ((midi_note - A4_MIDI_NOTE) / 12)
 
 
 class Voice:
     def __init__(self):
         self.is_active = False
+        self.wave = load_data_from_wav_file("./data/akwf/AKWF/AKWF_0002/AKWF_0139.wav")
+        self.frequency = 440
+
+        self.oscillator = WavetableOscillator(
+            self.wave, self.frequency, 1, sampling_rate=44100
+        )
 
     def start(self, note):
         self.is_active = True
+        self.oscillator.frequency = midi_note_to_frequency(note)
+        self.oscillator.amplitude = 1
+        # print (self.oscillator.frequency)
 
     def stop(self):
         self.is_active = False
 
     def render(self, frames):
-        pass
+        return self.oscillator.render(frames)
 
 
 class MonoSynth:
@@ -39,3 +50,6 @@ class MonoSynth:
             if event.midi_note == self.current_note:
                 self.voice.stop()
                 self.current_note = None
+
+    def render(self, frames):
+        return self.voice.render(frames)
